a522719d56685eb2a3033a32867e3759b755d17d
unknown <hamza>
2020-06-04 13:41:44 +0100

    added hamza

--- index.js
+++ index.js
@@ -1,34 +1,29 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-var mysql = require('mysql');
+var mysql = require("mysql");
 var resultat;
 var con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "hackeddetected",
-    database: "hopital"
-  });
-  
-con.connect(function(err) {
-    if (err) throw err;
-    console.log("Database is Connected!");
+  host: "localhost",
+  user: "root",
+  password: "hamza",
+  database: "hopital",
 });
 
+con.connect(function (err) {
+  if (err) throw err;
+  console.log("Database is Connected!");
+});
 
+app.get("/", (req, res) => {
+  con.query("select * from user", function (err, result, fields) {
+    if (err) throw err;
+    console.log(result);
+    resultat = result;
+  });
+  res.send(resultat);
+});
+console.log("ok11");
 
-
-app.get('/', (req,res)=> { 
-    con.query("select * from user", function (err, result, fields) {
-        if (err) throw err;
-        console.log(result);
-        resultat = result;
-      });
-    res.send(resultat);
-})
-
-
-
-
-app.listen(4000,()=>{
-    console.log("server is running successfully on port 4000 !");
-})
+app.listen(3001, () => {
+  console.log("server is running successfully on port 4000 !");
+});

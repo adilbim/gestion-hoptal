cb464283f1bd668cfa8de1083b1063c2a9a9f71b
adil bimzagh <abshow15@gmail.com>
2020-06-04 18:18:43 +0100

    add rendezvous stuff

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const express = require("express");
 const app = express();
 
 var patient = require("./routes/patient");
+var rendezVous = require("./routes/rendezVous");
 app.use("/api", patient);
+app.use("/api", rendezVous);
+
+
 
 app.listen(3001, () => {
-  console.log("server is running successfully on port 4000 !");
+  console.log("server is running successfully on port 3001 !");
 });
---
+++ routes/rendezVous.js
@@ -0,0 +1,89 @@
+var express = require("express");
+var router = express.Router();
+var bodyParser = require("body-parser");
+
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: false }));
+
+const { v4: uuidv4 } = require("uuid");
+
+//console.log(uuidv4());
+
+var mysql = require("mysql");
+var resultat;
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "hackeddetected",
+  database: "hopital",
+});
+
+con.connect(function (err) {
+  if (err) throw err;
+  console.log("Database is Connected!");
+});
+
+router.get("/RendezVous", (req, res) => {
+  var sql = "select * from rendezVous";
+  con.query(sql, (err, result) => {
+    if (err) throw err;
+    else {
+      res.send(result);
+    }
+  });
+});
+
+router.get("/rendezVous/patient/:idPatient", (req, res) => {
+    var sql = `select * from rendezVous where idPatient = ${req.params.idPatient};`;
+    con.query(sql, (err, result) => {
+      if (err) throw err;
+      else {
+        res.send(result);
+      }
+    });
+  });
+  
+router.get("/rendezVous/medecin/:idMedecin", (req,res)=> {
+   var sql = `select * from rendezvous where idMedecin = ${req.params.idMedecin}`;
+   con.query(sql, (err, result) => {
+    if (err) throw err;
+    else {
+      res.send(result);
+    }
+  });
+})  
+
+router.post("/RendezVous", (req, res) => {
+    //  {
+    //     dateR: '2020-05-25 12:45:55',
+    //     service: 'consultation',
+    //     presence: false,
+     //     prix: 255.0
+    //     date: '2020-05-25 12:45:55',
+    //     cheminDeBilan: 'fill it later',
+    //     idPatient: '1401',
+    //     idMedecin: '1102'
+    // }
+    var data = req.body;
+    data.id = uuidv4();
+    console.log(data);
+    var sql = `INSERT INTO rendezVous 
+    (id, dateR, service, presence, prix, date, cheminDeBilan, idMedecin, idPatient) VALUES 
+    ('${data.id}', '${data.dateR}', '${data.service}', ${data.presence}, ${data.prix}, '${data.date}' , '${data.cheminDeBilan}', '${data.idMedecin}', '${data.idPatient}');`;
+    con.query(sql, (err, result) => {
+      if (err) throw err;
+      else {
+        res.send(result);
+      }
+    });
+});  
+
+
+
+
+
+
+
+
+
+module.exports = router;
